33d60fbb67497a67a5bfc1c1a450ef1110859ddb
Kiash254 <codewithkiash@gmail.com>
2023-12-03 18:50:56 +0300

    added items on my db

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Package(models.Model):
+    TRACKING_STATUS_CHOICES = [
+        ('IN_TRANSIT', 'In Transit'),
+        ('OUT_FOR_DELIVERY', 'Out for Delivery'),
+        ('DELIVERED', 'Delivered'),
+        ('PICKED_UP', 'Picked Up'),
+    ]
+
+    tracking_number = models.CharField(max_length=200, unique=True)
+    status = models.CharField(max_length=20, choices=TRACKING_STATUS_CHOICES, default='IN_TRANSIT')
+    current_location = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.status
+
+class Notification(models.Model):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Notification for {self.package.tracking_number} to {self.user.username}'
--- tracker/views.py
+++ tracker/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .models import Package, Notification
 
-# Create your views here.
-def  Homeview(request):
-    
-    
-    context={}
-    
-    return render(request,'home.html',context)
+def Homeview(request):
+    packages = Package.objects.all()
+    notifications = Notification.objects.filter(user=request.user, read=False)
+    context = {
+        'packages': packages,
+        'notifications': notifications,
+    }
+    return render(request, 'home.html', context)
+
+def PackageDetailView(request, tracking_number):
+    package = Package.objects.get(tracking_number=tracking_number)
+    context = {
+        'package': package,
+    }
+    return render(request, 'package_detail.html', context)
+
+def MarkNotificationAsRead(request, notification_id):
+    notification = Notification.objects.get(id=notification_id)
+    notification.read = True
+    notification.save()
+    return redirect('home')
